d4c12a632c88aa8a916ffa0e35c343539770771d
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-02-28 09:27:50 -0300

    feat: add peopleList

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,67 @@
+import { ChangeEvent, useState } from 'react';
+
 import { useContagem } from './reducers/contagem';
+import { usePeopleList } from './reducers/peopleList';
 
 import Button from './components/Button';
 
 function App() {
 
-  const [contagem, contagemDispatch] = useContagem();
+  // const [contagem, contagemDispatch] = useContagem();
+  const [peopleList, peopleListDispatch] = usePeopleList();
+  const [nameInput, setNameInput] = useState('');
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNameInput(e.target.value);
+  }
+
+  const handleAdd = () => {
+    if (nameInput) {
+      peopleListDispatch({
+        type: 'ADD',
+        payload: {
+          name: nameInput
+        }
+      });
+      setNameInput('');
+    }
+  }
 
   return (
-    <div>
-      <Button
-        onClick={() => contagemDispatch({ type: 'ADD' })}
-      >Adicionar</Button>
-
-      <Button
-        onClick={() => contagemDispatch({ type: 'DEL' })}
-      >Remover</Button>
-
-      <Button
-        onClick={() => contagemDispatch({ type: 'RESET' })}
-      >Resetar</Button>
+    <>
 
       <div>
-        Valor: {contagem.count}
+        <input type="text" value={nameInput} onChange={handleInputChange} />
+        <Button onClick={handleAdd}>Adicionar</Button>
+
+        <hr />
+
+        Lista de Pessoas
+        <ul>
+          {peopleList.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
       </div>
-    </div>
+
+      {/* <div>
+        <Button
+          onClick={() => contagemDispatch({ type: 'ADD' })}
+        >Adicionar</Button>
+
+        <Button
+          onClick={() => contagemDispatch({ type: 'DEL' })}
+        >Remover</Button>
+
+        <Button
+          onClick={() => contagemDispatch({ type: 'RESET' })}
+        >Resetar</Button>
+
+        <div>
+          Valor: {contagem.count}
+        </div>
+      </div> */}
+    </>
   )
 }
 
---
+++ src/reducers/peopleList.ts
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+
+import { v4 } from 'uuid';
+
+type Person = {
+  id: string;
+  name: string;
+}
+
+type ActionType = {
+  type: string;
+  payload?: {
+    name?: string;
+    id?: string;
+  }
+}
+
+const initialState: Person[] = [];
+
+const reducer = (state: Person[], action: ActionType) => {
+  switch (action.type) {
+    case 'ADD':
+      if (action.payload?.name) {
+        const newState = [...state];
+        newState.push({
+          id: v4(),
+          name: action.payload?.name
+        });
+        return newState;
+      }
+
+    case 'DEL':
+      if (action.payload?.id) {
+        state = state.filter((item) => item.id !== action.payload?.id);
+      }
+
+    case 'ORDER':
+      state = state.sort((a, b) => (a.name > b.name) ? 1 : -1);
+
+    default:
+      break;
+  }
+
+  return state;
+}
+
+export const usePeopleList = () => {
+  return useReducer(reducer, initialState);
+}
