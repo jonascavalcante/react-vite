6dc60e2827108b60df59b7d3f2dc8e2e4a2a4c23
Jonas Cavalcante <jonascavalcantejc@gmail.com>
2022-02-28 08:48:11 -0300

    feat: useReducer as a hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,27 @@
-import { useReducer } from 'react';
+import { useContagem } from './reducers/contagem';
+
 import Button from './components/Button';
-
-type reducerState = {
-  count: number
-}
-
-type reducerAction = {
-  type: string
-}
-
-const initialState = {
-  count: 0
-};
-
-const reducer = (state: reducerState, action: reducerAction) => {
-  switch (action.type) {
-    case 'ADD':
-      return { ...state, count: state.count + 1 }
-
-    case 'DEL':
-      if (state.count > 0) {
-        return { ...state, count: state.count - 1 }
-      }
-
-    case 'RESET':
-      return initialState;
-
-    default:
-      break;
-  }
-
-  return state;
-}
 
 function App() {
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [contagem, contagemDispatch] = useContagem();
 
   return (
     <div>
       <Button
-        onClick={() => dispatch({ type: 'ADD' })}
+        onClick={() => contagemDispatch({ type: 'ADD' })}
       >Adicionar</Button>
 
       <Button
-        onClick={() => dispatch({ type: 'DEL' })}
+        onClick={() => contagemDispatch({ type: 'DEL' })}
       >Remover</Button>
 
       <Button
-        onClick={() => dispatch({ type: 'RESET' })}
+        onClick={() => contagemDispatch({ type: 'RESET' })}
       >Resetar</Button>
 
       <div>
-        Valor: {state.count}
+        Valor: {contagem.count}
       </div>
     </div>
   )
